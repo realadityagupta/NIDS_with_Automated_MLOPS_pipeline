8633d57f284701d72fa0d5ed85a0eac54d071eaa
Aditya gupta <ajaygupta995566@gmail.com>
2026-08-20 18:17:45 +0530

    ETL pipeline finish

---
+++ push_data.py
@@ -0,0 +1,65 @@
+# we are going to create a new file called push_data.py which will be used to push the data from csv file to mongodb database
+import os
+import sys
+import json
+
+from dotenv import load_dotenv
+load_dotenv()
+
+MONGO_DB_URL=os.getenv("MONGO_DB_URL")
+
+# certifi is a Python package that provides a collection of trusted SSL/TLS certificates.
+
+# Think of it as a list of websites that Python can trust when making HTTPS connections.
+import certifi
+ca=certifi.where()
+
+import pandas as pd
+import numpy as np
+import pymongo
+from networkSecurity.exceptionHandling.exception import NetworkSecurityException
+from networkSecurity.logging.logger import logging
+
+class NetworkDataExtract():
+    def __init__(self):
+        try:
+            pass
+        except Exception as e:
+            raise NetworkSecurityException(e,sys)
+
+    def csv_to_json_convertor(self,file_path):
+        try:
+            data=pd.read_csv(file_path)
+            data.reset_index(drop=True,inplace=True)
+            records=list(json.loads(data.T.to_json()).values())
+            return records
+
+        except Exception as e:
+            raise NetworkSecurityException(e,sys)
+    
+    def insert_data_mongodb(self,records,database,collection):
+        try:
+            self.database=database
+            self.collection=collection
+            self.records=records
+
+            self.mongo_client=pymongo.MongoClient(MONGO_DB_URL)
+            self.database=self.mongo_client[self.database]
+
+            self.collection=self.database[self.collection]
+            self.collection.insert_many(self.records)
+            return(len(self.records))
+        except Exception as e:
+            raise NetworkSecurityException(e,sys)
+
+
+# executing the pipeline        
+if __name__=='__main__': 
+    FILE_PATH="Network_Data/phisingData.csv"
+    DATABASE="Aditya_AI_database"
+    Collection="NetworkData"
+    networkobj=NetworkDataExtract()
+    records=networkobj.csv_to_json_convertor(file_path=FILE_PATH)
+    print(records)
+    no_of_records=networkobj.insert_data_mongodb(records,DATABASE,Collection)
+    print(no_of_records)
---
+++ test_mongodb.py
@@ -0,0 +1,19 @@
+from pymongo.mongo_client import MongoClient
+import os
+
+from dotenv import load_dotenv
+load_dotenv()
+
+MONGO_DB_URL=os.getenv("MONGO_DB_URL")
+
+uri = MONGO_DB_URL
+
+# Create a new client and connect to the server
+client = MongoClient(uri)
+
+# Send a ping to confirm a successful connection
+try:
+    client.admin.command('ping')
+    print("Pinged your deployment. You successfully connected to MongoDB!")
+except Exception as e:
+    print(e)
